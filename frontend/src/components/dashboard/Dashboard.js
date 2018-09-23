b3e51e3c564043bb1e10e90e1025a88f5862d835
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-09-23 16:39:04 +0530

    added check for admin view

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -7,7 +7,8 @@ class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      data: [],
+      admin: []
     }
     this.logout = this.logout.bind(this);
   }
@@ -25,6 +26,16 @@ class Dashboard extends Component {
           // console.log(response.status);
           response.json())
       .then(data => this.setState({data: data}));
+
+    fetch('http://localhost:8000/type', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(admin => this.setState({admin: admin}));
 
   }
 
@@ -58,16 +69,20 @@ class Dashboard extends Component {
                   <div className="navbar-header">
                   </div>
                   <ul className="nav navbar-nav">
-                    <li><Link to={'/ViewUsers'}>View Users</Link></li>
+                    {this.state.admin.type=="admin" && 
+                      <li><Link to={'/ViewUsers'}>View Users</Link></li>
+                    }
                   </ul>
                   <ul className="nav navbar-nav navbar-right">
                     <li><button onClick={this.logout} className="btn btn-danger btn-lg btn-login btn-block">Log out</button></li>
                   </ul>
                 </div>
               </nav>
-              <Switch>
+              {this.state.admin.type=="admin" && 
+                <Switch>
                   <Route exact path='/ViewUsers' component={ViewUsers} />
-              </Switch>
+                </Switch>
+              }
             </div>
           </Router>
         </div>
--- frontend/src/components/dashboard/ViewUsers.js
+++ frontend/src/components/dashboard/ViewUsers.js
@@ -8,27 +8,27 @@ class ViewUsers extends Component {
       data: [],
       deleteId: "",
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRChange = this.handleRChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleRChange = this.handleRChange.bind(this);
   }
 
-  handleRChange(event) {
-    this.setState({deleteId: event.target.value});
-  }
+//   handleRChange(event) {
+//     this.setState({deleteId: event.target.value});
+//   }
 
-  handleSubmit (event) {
-    event.preventDefault();
-    fetch('http://localhost:8000/user/' + this.state.deleteId, {
-      method: 'DELETE',
-      credentials: 'include',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.state.deleteId),
-    })
-      .then(response => this.setState({deleteId: ""}));
-          window.location.reload();
- }
+//   handleSubmit (event) {
+//     event.preventDefault();
+//     fetch('http://localhost:8000/user/' + this.state.deleteId, {
+//       method: 'DELETE',
+//       credentials: 'include',
+//       headers: {
+//           'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(this.state.deleteId),
+//     })
+//       .then(response => this.setState({deleteId: ""}));
+//           window.location.reload();
+//  }
 
   componentDidMount() {
     fetch('http://localhost:8000/users', {
@@ -48,14 +48,14 @@ class ViewUsers extends Component {
     return (
       <div className="App">
 
-        <form onSubmit={this.handleSubmit}>
+        {/* <form onSubmit={this.handleSubmit}> */}
         <table className="table-hover">
           <thead>
             <tr>
               <th>ID</th>
               <th>Username</th>
               <th>Type</th>
-              <th>Delete</th>
+              {/* <th>Delete</th> */}
             </tr>
           </thead>
           <tbody>{this.state.data.map((item, key)=> {
@@ -64,15 +64,15 @@ class ViewUsers extends Component {
                       <td>{item.id}</td>
                       <td>{item.username}</td>
                       <td>{item.type}</td>
-                      <td><input type="radio" name="radioInput" value={item.id} onChange={this.handleRChange}/></td>
+                      {/* <td><input type="radio" name="radioInput" value={item.id} onChange={this.handleRChange}/></td> */}
                   </tr>
                 )
              })}
           </tbody>
        </table>
        <br></br>
-        <button type="submit" className="btn btn-default">Delete</button>
-       </form>
+        {/* <button type="submit" className="btn btn-default">Delete</button>
+       </form> */}
       </div>
     );
   }
