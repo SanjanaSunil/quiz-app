17080eb598fb7d577402148dd9d4084a006b4b3d
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-09-23 14:32:06 +0530

    created controllers

--- backend/src/api/api.go
+++ backend/src/api/api.go
@@ -3,11 +3,9 @@ package api
 import (
 	"fmt"
 	"log"
-	"encoding/json"
 	controller "./controller"
 	model "./model"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
 	"net/http"
 	"github.com/rs/cors"
 	"github.com/jinzhu/gorm"
@@ -16,11 +14,6 @@ import (
 
 var db *gorm.DB
 var err error
-
-var (
-	key = []byte("super-secret-key")
-	store = sessions.NewCookieStore(key)
-)
 
 var router = mux.NewRouter()
 
@@ -53,103 +46,29 @@ func Run() {
 }
 
 func Authenticate(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		controller.ErrorResponse(w, http.StatusForbidden, "Forbidden")
-		return
-	}
-	if str, ok := session.Values["user"].(string); ok {
-		controller.JSONResponse(w, http.StatusOK, map[string]string{"username": str})
-	} else {
-		controller.ErrorResponse(w, http.StatusNotFound, err.Error())
-	}
+	controller.Authenticate(db, w, r)
 }
 
-func GetUser(response http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	id := vars["id"]
-	var user model.User
-	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusNotFound, err.Error())
-	 	fmt.Println(err)
-	} else {
-		controller.JSONResponse(response, http.StatusOK, user)
-  	}
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	controller.GetUser(db, w, r)
 }
  
-func GetUsers(response http.ResponseWriter, request *http.Request) {
-	var users []model.User
-	if err := db.Find(&users).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusNotFound, err.Error())
-		fmt.Println(err)
-  	} else {
-		controller.JSONResponse(response, http.StatusOK, users)
-	}
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	controller.GetUsers(db, w, r)
 }
 
-func DeleteUser(response http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	id := vars["id"]
-	var user model.User
-	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusNotFound, err.Error())
-		fmt.Println(err)
-   	}
-   	if err := db.Delete(&user).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusInternalServerError, err.Error())
-		fmt.Println(err)
-		return
-	}
-	controller.JSONResponse(response, http.StatusNoContent, nil)
+func DeleteUser(w http.ResponseWriter, r*http.Request) {
+	controller.DeleteUser(db, w, r)
 }
 
-func SignUp(response http.ResponseWriter, request *http.Request) {
-	session, _ := store.Get(request, "cookie-name")
-	var user model.User
-	decoder := json.NewDecoder(request.Body)
-	if err := decoder.Decode(&user); err != nil {
-		controller.ErrorResponse(response, http.StatusBadRequest, err.Error())
-		return
-	}
-	defer request.Body.Close()
- 	
-	if err := db.Save(&user).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusInternalServerError, err.Error())
-		return
-	}
-	session.Values["authenticated"] = true
-	session.Values["user"] = user.Username
-	session.Save(request, response)
-	controller.JSONResponse(response, http.StatusOK, user)
+func SignUp(w http.ResponseWriter, r *http.Request) {
+	controller.SignUp(db, w, r)
 }
 
-func SignIn(response http.ResponseWriter, request *http.Request) {
-	session, _ := store.Get(request, "cookie-name")
-	var user model.User
-	decoder := json.NewDecoder(request.Body)
-	if err := decoder.Decode(&user); err != nil {
-		controller.ErrorResponse(response, http.StatusBadRequest, err.Error())
-		return
-	}
-	defer request.Body.Close()
- 
-	if err := db.Where("username = ? AND password = ?", user.Username, user.Password).First(&user).Error; err != nil {
-		controller.ErrorResponse(response, http.StatusNotFound, err.Error())
-		fmt.Println(err)
-   	} else {
-		session.Values["authenticated"] = true
-		session.Values["user"] = user.Username
-		session.Save(request, response)
-		controller.JSONResponse(response, http.StatusOK, user)
-	}
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	controller.SignIn(db, w, r)
 }
 
-func Logout(response http.ResponseWriter, request *http.Request) {
-	session, _ := store.Get(request, "cookie-name")
-
-	session.Options = &sessions.Options{
-		MaxAge:	-1,
-		HttpOnly: true,
-	}
-	session.Save(request, response)
+func Logout(w http.ResponseWriter, r *http.Request) {
+	controller.Logout(db, w, r)
 }
---
+++ backend/src/api/controller/user.go
@@ -0,0 +1,121 @@
+package controller
+
+import (
+	"fmt"
+	model "../model"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" 
+)
+
+var err error
+
+var (
+	key = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+func Authenticate(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		ErrorResponse(w, http.StatusForbidden, "Forbidden")
+		return
+	}
+	if str, ok := session.Values["user"].(string); ok {
+		JSONResponse(w, http.StatusOK, map[string]string{"username": str})
+	} else {
+		ErrorResponse(w, http.StatusNotFound, err.Error())
+	}
+}
+
+func GetUser(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+	var user model.User
+	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		ErrorResponse(response, http.StatusNotFound, err.Error())
+	 	fmt.Println(err)
+	} else {
+		JSONResponse(response, http.StatusOK, user)
+  	}
+}
+ 
+func GetUsers(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	var users []model.User
+	if err := db.Find(&users).Error; err != nil {
+		ErrorResponse(response, http.StatusNotFound, err.Error())
+		fmt.Println(err)
+  	} else {
+		JSONResponse(response, http.StatusOK, users)
+	}
+}
+
+func DeleteUser(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+	var user model.User
+	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		ErrorResponse(response, http.StatusNotFound, err.Error())
+		fmt.Println(err)
+   	}
+   	if err := db.Delete(&user).Error; err != nil {
+		ErrorResponse(response, http.StatusInternalServerError, err.Error())
+		fmt.Println(err)
+		return
+	}
+	JSONResponse(response, http.StatusNoContent, nil)
+}
+
+func SignUp(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	session, _ := store.Get(request, "cookie-name")
+	var user model.User
+	decoder := json.NewDecoder(request.Body)
+	if err := decoder.Decode(&user); err != nil {
+		ErrorResponse(response, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer request.Body.Close()
+ 	
+	if err := db.Save(&user).Error; err != nil {
+		ErrorResponse(response, http.StatusInternalServerError, err.Error())
+		return
+	}
+	session.Values["authenticated"] = true
+	session.Values["user"] = user.Username
+	session.Save(request, response)
+	JSONResponse(response, http.StatusOK, user)
+}
+
+func SignIn(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	session, _ := store.Get(request, "cookie-name")
+	var user model.User
+	decoder := json.NewDecoder(request.Body)
+	if err := decoder.Decode(&user); err != nil {
+		ErrorResponse(response, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer request.Body.Close()
+ 
+	if err := db.Where("username = ? AND password = ?", user.Username, user.Password).First(&user).Error; err != nil {
+		ErrorResponse(response, http.StatusNotFound, err.Error())
+		fmt.Println(err)
+   	} else {
+		session.Values["authenticated"] = true
+		session.Values["user"] = user.Username
+		session.Save(request, response)
+		JSONResponse(response, http.StatusOK, user)
+	}
+}
+
+func Logout(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
+	session, _ := store.Get(request, "cookie-name")
+
+	session.Options = &sessions.Options{
+		MaxAge:	-1,
+		HttpOnly: true,
+	}
+	session.Save(request, response)
+}
