a3b23afba3689b7e5f78adff723419576a6d4693
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-09-25 11:49:33 +0530

    added options

--- backend/src/api/controller/quiz.go
+++ backend/src/api/controller/quiz.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gorilla/mux"
 	"encoding/json"
 	"net/http"
+	"io/ioutil"
+	"bytes"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite" 
 )
@@ -51,13 +53,29 @@ func CreateQuizGenre(db *gorm.DB, response http.ResponseWriter, request *http.Re
 
 func CreateQuizQuestion(db *gorm.DB, response http.ResponseWriter, request *http.Request) {
 	var question model.Question
-	decoder := json.NewDecoder(request.Body)
+	buf, _ := ioutil.ReadAll(request.Body)
+	rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+	rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
+	decoder := json.NewDecoder(rdr1)
 	if err := decoder.Decode(&question); err != nil {
 		ErrorResponse(response, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer request.Body.Close()
 	if err := db.Save(&question).Error; err != nil {
+		ErrorResponse(response, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var option model.Option
+	request.Body = rdr2
+	decoder = json.NewDecoder(request.Body)
+	if err := decoder.Decode(&option); err != nil {
+		ErrorResponse(response, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer request.Body.Close()
+	option.QuestionId = question.ID
+	if err := db.Save(&option).Error; err != nil {
 		ErrorResponse(response, http.StatusInternalServerError, err.Error())
 		return
 	}
--- backend/src/api/model/quiz.go
+++ backend/src/api/model/quiz.go
@@ -10,3 +10,10 @@ type Question struct {
 	GenreId uint `json:"genre_id"`
 	Question string `json:"question"`
 }
+
+type Option struct {
+	ID uint `json:"id"`
+	QuestionId uint `json:"question_id"`
+	Option string `json:"option"`
+	Answer string `json:"answer"`
+}
